a3ca123a048d1caa42535fdbc3230abc5c4a02eb
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-03-03 23:06:10 +0100

    Add Navbar component

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,44 @@
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import React from "react";
+
+export const Navbar = () => {
+    return (
+    <SideNav
+    onSelect={(selected) => {
+        // Eventkey value passed to the onSelect handler,
+        // useful for identifying the selected navigation item.
+    }}
+        >
+    <Toggle />
+    <Nav defaultSelected="home">
+        <NavItem eventKey="home">
+            <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Home
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="income">
+            <NavIcon>
+                <i className="fa fa-plus" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Add Income
+            </NavText>
+        </NavItem>
+        <NavItem eventKey="cost">
+            <NavIcon>
+                <i className="fa fa-minus" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+                Add Cost
+            </NavText>
+        </NavItem>
+    </Nav>
+</SideNav>
+)
+};
+
+export default Navbar;
