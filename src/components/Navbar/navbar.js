b94364ed3603ce6281f6e31745dcdd4e8c043b3f
wory04 <voroseszterveronika@gmail.com>
2020-03-05 00:35:03 +0100

    add TransactionModal to Nav

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -6,9 +6,13 @@ import SideNav, {
   NavText
 } from "@trendmicro/react-sidenav";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
-import React from "react";
+import React, { useState } from "react";
+import TransactionModal from "../Modal/transaction-modal";
 
 export const Navbar = () => {
+  const [transactionmodalDisplay, setTransactionModalDisplay] = useState(false);
+  const [transactionType, setTransactionType] = useState();
+
   return (
     <SideNav
       onSelect={selected => {
@@ -24,19 +28,36 @@ export const Navbar = () => {
           </NavIcon>
           <NavText>Home</NavText>
         </NavItem>
-        <NavItem eventKey="income">
+        <NavItem
+          eventKey="income"
+          onClick={() => {
+            setTransactionType("income");  
+            setTransactionModalDisplay(true);
+        }}
+        >
           <NavIcon>
             <i className="fa fa-plus" style={{ fontSize: "1.75em" }} />
           </NavIcon>
           <NavText>Add Income</NavText>
         </NavItem>
-        <NavItem eventKey="cost">
+        <NavItem 
+            eventKey="cost"
+            onClick={() => {
+                setTransactionType("cost");  
+                setTransactionModalDisplay(true);
+        }}
+        >
           <NavIcon>
             <i className="fa fa-minus" style={{ fontSize: "1.75em" }} />
           </NavIcon>
           <NavText>Add Cost</NavText>
         </NavItem>
       </Nav>
+      <TransactionModal
+        display={transactionmodalDisplay}
+        handleClose={() => setTransactionModalDisplay(false)}
+        transactionType={transactionType}
+      />
     </SideNav>
   );
 };
