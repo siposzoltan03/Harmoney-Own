eaf9cf7d574b4738d2130af941717b2f04db976f
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-04 15:24:52 +0100

    Add basic layout grid to App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import './App.css';
-import Navbar from "../../components/Navbar/navbar";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import "./App.css";
+import SideBar from "../../components/Navbar/navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import TopBar from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App">
-      <h1>Harmoney</h1>
-        <Navbar/>
+      <TopBar className="justify-content-center ml-64 navBar-shadow">
+        <h1 id="topBar-title">HarMoney</h1>
+      </TopBar>
+      <SideBar />
+      <Container>
+        <Row>
+          <Col lg={true}></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
