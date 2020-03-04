37078583912a674fd7170daebbecb5bb2fc690c4
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-04 15:23:09 +0100

    Run prettier on navbar.js

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,44 +1,44 @@
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import SideNav, {
+  Toggle,
+  Nav,
+  NavItem,
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 import React from "react";
 
 export const Navbar = () => {
-    return (
+  return (
     <SideNav
-    onSelect={(selected) => {
+      onSelect={selected => {
         // Eventkey value passed to the onSelect handler,
         // useful for identifying the selected navigation item.
-    }}
-        >
-    <Toggle />
-    <Nav defaultSelected="home">
+      }}
+    >
+      <Toggle />
+      <Nav defaultSelected="home">
         <NavItem eventKey="home">
-            <NavIcon>
-                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Home
-            </NavText>
+          <NavIcon>
+            <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} />
+          </NavIcon>
+          <NavText>Home</NavText>
         </NavItem>
         <NavItem eventKey="income">
-            <NavIcon>
-                <i className="fa fa-plus" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Add Income
-            </NavText>
+          <NavIcon>
+            <i className="fa fa-plus" style={{ fontSize: "1.75em" }} />
+          </NavIcon>
+          <NavText>Add Income</NavText>
         </NavItem>
         <NavItem eventKey="cost">
-            <NavIcon>
-                <i className="fa fa-minus" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Add Cost
-            </NavText>
+          <NavIcon>
+            <i className="fa fa-minus" style={{ fontSize: "1.75em" }} />
+          </NavIcon>
+          <NavText>Add Cost</NavText>
         </NavItem>
-    </Nav>
-</SideNav>
-)
+      </Nav>
+    </SideNav>
+  );
 };
 
 export default Navbar;
