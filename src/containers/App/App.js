1ad49c76bab0a90b1e0c95bd2c3ec86e66ec09e9
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-04 11:10:20 +0100

    Import React-Bootstrap grid components

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Navbar from "../../components/Navbar/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
