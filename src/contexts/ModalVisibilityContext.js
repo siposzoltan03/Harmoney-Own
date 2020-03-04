ff41cd640950ec300c64ca9f49ff8f19481f0164
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-03-05 00:16:28 +0100

    Add login data to context

--- src/contexts/ModalVisibilityContext.js
+++ src/contexts/ModalVisibilityContext.js
@@ -4,10 +4,11 @@ export const ModalVisibilityContext = React.createContext();
 
 export const ModalVisibilityContextProvider = props => {
     const [registrationModalIsVisible, setRegistrationModalIsVisible] = useState(false);
+    const [loginModalIsVisible, setLoginModalIsVisible] = useState(false);
 
     return (
         <ModalVisibilityContext.Provider
-            value={{ regModal: [registrationModalIsVisible, setRegistrationModalIsVisible] }}
+            value={{ regModal: [registrationModalIsVisible, setRegistrationModalIsVisible], logModal: [loginModalIsVisible, setLoginModalIsVisible] }}
         >
             {props.children}
         </ModalVisibilityContext.Provider>
