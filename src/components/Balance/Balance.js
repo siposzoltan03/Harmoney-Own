4927f36209c5ac369409ebafcd5df0000f5bbd9a
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-03-05 14:16:53 +0100

    Add db data to display balance feature

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, {useContext} from "react";
+import "./Balance.css";
 import './Balance.css';
 import Card from "react-bootstrap/Card";
-import NumberFormater from '../../utils/NumberFormater'
+import NumberFormater from "../../utils/NumberFormater";
+import {TransactionContext} from "../../contexts/TransactionContext";
+
+let sum = 0;
 
 const Balance = () => {
+    const appContext = useContext(TransactionContext);
+    const transactions = appContext.transactions[0];
+    transactions.forEach(function (transaction) {
+        transaction.direction === 'Income' ? sum += transaction.amount : sum -= transaction.amount;
+    });
     return (
         <Card>
             <Card.Body>
                 <Card.Title>Actual Balance</Card.Title>
-                <Card.Text>
-                    {NumberFormater.formatBalance(Value)}
-                </Card.Text>
+                <Card.Text>{NumberFormater.formatBalance(sum)} Ft</Card.Text>
             </Card.Body>
         </Card>
     );
 };
-
-const Value = 386450;
 
 export default Balance;
