1ec05b96888c65d86a0b076bbd0edd74115b510b
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-03-05 11:46:41 +0100

    Implement Transaction context

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,21 +5,25 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import TopBar from "react-bootstrap/Nav";
+import {TransactionProvider} from "../../contexts/TransactionContext";
 
 function App() {
-  return (
-    <div className="App">
-      <TopBar className="justify-content-center ml-64 navBar-shadow">
-        <h1 id="topBar-title">HarMoney</h1>
-      </TopBar>
-      <SideBar />
-      <Container>
-        <Row>
-          <Col lg={true}></Col>
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <TransactionProvider>
+            <div className="App">
+                <TopBar className="justify-content-center ml-64 navBar-shadow">
+                    <h1 id="topBar-title">HarMoney</h1>
+                </TopBar>
+                <SideBar/>
+                <Container>
+                    <Row>
+                        <Col lg={true}>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </TransactionProvider>
+    );
 }
 
 export default App;
