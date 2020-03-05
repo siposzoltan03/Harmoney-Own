7b6fcf5a03d0b919cf30e8d2e735bd30cf87522d
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-03-05 17:39:17 +0100

    Add db data to display chart feature

--- src/components/Charts/DashboardChart.js
+++ src/components/Charts/DashboardChart.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Doughnut } from 'react-chartjs-2';
+import Card from "react-bootstrap/Card";
+import NumberFormater from "../../utils/NumberFormater";
+import {TransactionContext} from "../../contexts/TransactionContext";
 
-const income = 1600;
-const costs = 800;
+let Income = 0;
+let Expenditure = 0;
 
 const DashboardChart = () => {
+    const appContext = useContext(TransactionContext);
+    const transactions = appContext.transactions[0];
+    Income = 0;
+    Expenditure = 0;
+    transactions.forEach(function (transaction) {
+        if (transaction.direction === 'Income') {
+            Income += transaction.amount;
+        }else{
+            Expenditure += transaction.amount;
+        }
+    });
+
+    const data = {
+        datasets: [{
+            data: [ Income  ,Expenditure],
+            backgroundColor:[
+                '#BFA5A3',
+                '#600000'
+            ],
+            options: {
+                plugins:{
+                fontSize: "120px",
+                color: 'yellow'
+                }
+            }
+        }],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Income',
+            'Expenditure',
+        ],
+    };
+
     return (
-        <Doughnut data={data}/>
+        <Card>
+            <Card.Body>
+                <Card.Title>Cashflow ratio</Card.Title>
+                <Doughnut data={data}/>
+            </Card.Body>
+        </Card>
     );
 };
 
-const data = {
-    datasets: [{
-        data: [income, costs],
-        backgroundColor:[
-            'white',
-            'black'
-        ]
-    }],
 
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Income',
-        'Costs'
-    ],
-};
 
 
 
