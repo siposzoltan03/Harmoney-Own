349626b39f1c54cf1895cb49519811c053771abb
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-05 11:28:04 +0100

    Solve merge conflict

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -6,14 +6,33 @@ import SideNav, {
   NavText
 } from "@trendmicro/react-sidenav";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
-import React from "react";
+
+import React, { useState, useContext } from "react";
+import { ModalVisibilityContext } from "../../contexts/ModalVisibilityContext";
+import TransactionModal from "../Modal/transaction-modal";
 
 export const Navbar = () => {
+  const { regModal, logModal } = useContext(ModalVisibilityContext);
+  const setRegistrationModalIsVisible = regModal[1];
+  const setLoginModalIsVisible = logModal[1];
+  const [transactionmodalDisplay, setTransactionModalDisplay] = useState(false);
+  const [transactionType, setTransactionType] = useState();
+
   return (
     <SideNav
       onSelect={selected => {
         // Eventkey value passed to the onSelect handler,
         // useful for identifying the selected navigation item.
+        switch (selected) {
+          case "user-registration":
+            setRegistrationModalIsVisible(true);
+            break;
+          case "user-login":
+            setLoginModalIsVisible(true);
+            break;
+          default:
+          // code block
+        }
       }}
     >
       <Toggle />
@@ -24,19 +43,52 @@ export const Navbar = () => {
           </NavIcon>
           <NavText>Home</NavText>
         </NavItem>
-        <NavItem eventKey="income">
+
+        <NavItem eventKey="user-registration">
+          <NavIcon>
+            <i className="fas fa-user-plus" style={{ fontSize: "1.75em" }} />
+          </NavIcon>
+          <NavText>Registration</NavText>
+        </NavItem>
+
+        <NavItem eventKey="user-login">
+          <NavIcon>
+            <i className="fas fa-user-check" style={{ fontSize: "1.75em" }} />
+          </NavIcon>
+          <NavText>Login</NavText>
+        </NavItem>
+
+        <NavItem
+          eventKey="income"
+          onClick={() => {
+            setTransactionType("income");
+            setTransactionModalDisplay(true);
+          }}
+        >
           <NavIcon>
             <i className="fa fa-plus" style={{ fontSize: "1.75em" }} />
           </NavIcon>
           <NavText>Add Income</NavText>
         </NavItem>
-        <NavItem eventKey="cost">
+
+        <NavItem
+          eventKey="cost"
+          onClick={() => {
+            setTransactionType("cost");
+            setTransactionModalDisplay(true);
+          }}
+        >
           <NavIcon>
             <i className="fa fa-minus" style={{ fontSize: "1.75em" }} />
           </NavIcon>
           <NavText>Add Cost</NavText>
         </NavItem>
       </Nav>
+      <TransactionModal
+        display={transactionmodalDisplay}
+        handleClose={() => setTransactionModalDisplay(false)}
+        transactionType={transactionType}
+      />
     </SideNav>
   );
 };
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,48 +5,55 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import TopBar from "react-bootstrap/Nav";
+import {ModalVisibilityContextProvider} from "../../contexts/ModalVisibilityContext";
+import RegistrationModal from "../../components/RegistrationModal/RegistrationModal";
+import LoginModal from "../../components/LoginModal/LoginModal";
 import Balance from "../../components/Balance/Balance";
 import Footer from "../../components/Footer/footer";
 
-const colPosition = { span: true, offset: 3 };
+const colPosition = {span: true, offset: 3};
 
 function App() {
-  return (
-    <div className="App">
-      <TopBar className="justify-content-center ml-32">
-        <h1 id="topBar-title">Harmoney</h1>
-      </TopBar>
-      <SideBar />
-      <Container>
-        <Row>
-          <Col
-            lg={colPosition}
-            md={colPosition}
-            sm={colPosition}
-            xl={colPosition}
-            xs={colPosition}
-          >
-            <Balance />
-          </Col>
-        </Row>
-      </Container>
-      <Footer>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://icons8.com/icons/set/finance-document---v2"
-        >
-          Finance Document icon
-        </a>{" "}
-        icon by{" "}
-        <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">
-          Icons8
-        </a>
-        <br />
-        Copyright 2020
-      </Footer>
-    </div>
-  );
+    return (
+        <ModalVisibilityContextProvider>
+            <div className="App">
+                <TopBar className="justify-content-center ml-32">
+                    <h1 id="topBar-title">Harmoney</h1>
+                </TopBar>
+                <SideBar/>
+                <Container>
+                    <Row>
+                        <Col
+                            lg={colPosition}
+                            md={colPosition}
+                            sm={colPosition}
+                            xl={colPosition}
+                            xs={colPosition}
+                        >
+                            <Balance/>
+                        </Col>
+                    </Row>
+                </Container>
+                <RegistrationModal/>
+                <LoginModal/>
+                <Footer>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://icons8.com/icons/set/finance-document---v2"
+                    >
+                        Finance Document icon
+                    </a>{" "}
+                    icon by{" "}
+                    <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">
+                        Icons8
+                    </a>
+                    <br/>
+                    Copyright 2020
+                </Footer>
+            </div>
+        </ModalVisibilityContextProvider>
+    );
 }
 
 export default App;
