a76d52afa6b1909c6a6f5afc703b0d72b237068b
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-03-06 00:42:25 +0100

    Fix balance sum

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -5,11 +5,11 @@ import Card from "react-bootstrap/Card";
 import NumberFormater from "../../utils/NumberFormater";
 import {TransactionContext} from "../../contexts/TransactionContext";
 
-let sum = 0;
 
 const Balance = () => {
     const appContext = useContext(TransactionContext);
     const transactions = appContext.transactions[0];
+    let sum = 0;
     transactions.forEach(function (transaction) {
         transaction.direction === 'Income' ? sum += transaction.amount : sum -= transaction.amount;
     });
