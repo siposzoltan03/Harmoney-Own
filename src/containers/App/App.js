c58816fe776b8aa0126e6b8ec7be7880b5992f86
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-05 10:21:10 +0100

    Add Footer to App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,29 +6,47 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import TopBar from "react-bootstrap/Nav";
 import Balance from "../../components/Balance/Balance";
+import Footer from "../../components/Footer/footer";
+
+const colPosition = { span: true, offset: 3 };
 
 function App() {
-    return (
-        <div className="App">
-            <TopBar className="justify-content-center ml-32 navBar-shadow">
-                <h1 id="topBar-title">Harmoney</h1>
-            </TopBar>
-            <SideBar id="sideBar"/>
-            <Container >
-                <Row>
-                    <Col
-                        lg={{ span: true, offset: 3}}
-                         md={{ span: true, offset: 3}}
-                         sm={{ span: true, offset: 3}}
-                         xl={{ span: true, offset: 3}}
-                         xs={{ span: true, offset: 3}}
-                    >
-                        <Balance/>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <TopBar className="justify-content-center ml-32">
+        <h1 id="topBar-title">Harmoney</h1>
+      </TopBar>
+      <SideBar />
+      <Container>
+        <Row>
+          <Col
+            lg={colPosition}
+            md={colPosition}
+            sm={colPosition}
+            xl={colPosition}
+            xs={colPosition}
+          >
+            <Balance />
+          </Col>
+        </Row>
+      </Container>
+      <Footer>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://icons8.com/icons/set/finance-document---v2"
+        >
+          Finance Document icon
+        </a>{" "}
+        icon by{" "}
+        <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">
+          Icons8
+        </a>
+        <br />
+        Copyright 2020
+      </Footer>
+    </div>
+  );
 }
 
 export default App;
