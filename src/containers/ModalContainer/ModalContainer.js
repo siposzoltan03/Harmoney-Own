4f29e54fb91eb93e7b317a2f3770edbfd2f70410
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-17 11:11:42 +0100

    Merge branch 'development' of C:\Users\markp\Desktop\School\Codecool\4. - C# .NET\Enterprise - Harmoney\Frontend with conflicts.

--- src/containers/ModalContainer/ModalContainer.js
+++ src/containers/ModalContainer/ModalContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RegistrationModal from '../../components/Modals/RegistrationModal/RegistrationModal';
-import LoginModal from '../../components/Modals/LoginModal/LoginModal';
 import TransactionModal from '../../components/Modals/TransactionModal/TransactionModal';
+import LoginModal from "../../components/Modals/LoginModal/LoginModal";
 
 
 export default function ModalContainer() {
