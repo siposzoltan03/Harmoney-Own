a587b61d6754df9846ff20987df63a790af2f9a7
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-17 13:14:38 +0100

    Replace footer content to Footer component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -24,21 +24,7 @@ function App() {
                 <SideBar/>
                 <Dashboard/>
                 <ModalContainer/>
-                <Footer className="ml-32">
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://icons8.com/icons/set/finance-document---v2"
-                    >
-                        Finance Document icon
-                    </a>{" "}
-                    icon by{" "}
-                    <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">
-                        Icons8
-                    </a>
-                    <br/>
-                    Copyright 2020
-                </Footer>
+                <Footer className="ml-32" />
             </div>
         </ModalVisibilityContextProvider>
         </UserProvider>
