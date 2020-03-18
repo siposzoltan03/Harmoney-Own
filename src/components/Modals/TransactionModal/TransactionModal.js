25e2bf6e4d68f265123d867be6288439cf551a2d
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-03-18 20:05:13 +0100

    add validation

--- src/components/Modals/TransactionModal/TransactionModal.js
+++ src/components/Modals/TransactionModal/TransactionModal.js
@@ -9,7 +9,7 @@ import { TransactionContext } from "../../../contexts/TransactionContext";
 export default function TransactionModal(props) {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState(new Date());
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState("");
   const [frequency, setFrequency] = useState("Single");
 
   const { transactionModal, transactionModalType } = useContext(ModalVisibilityContext);
@@ -20,18 +20,91 @@ export default function TransactionModal(props) {
   const postTransaction = transactionContext.postTransaction;
 
   const saveChanges = () => {
-    const jsonData = transactionToJson(title, date, amount, frequency, transactionType);
-    postTransaction(jsonData);
-    closeModal();
+    const titleIsValid = validTitle();
+    const dateIsValid = validDate();
+    const amountIsValid = validAmount();
+    let submittable = titleIsValid && dateIsValid && amountIsValid;
+    if (submittable) {
+      const jsonData = transactionToJson(title, date, parseInt(amount), frequency, transactionType);
+      postTransaction(jsonData);
+      closeModal();
+    }
   };
 
   const closeModal = () => {
     setTransactionModalIsVisible(false);
     setTitle("");
     setDate(new Date());
-    setAmount(0);
+    setAmount("");
     setFrequency("Single");
   }
+
+  const validateTitle = (e) => {
+    setTitle(e.target.value);
+    validTitle();
+  }
+
+  const validTitle = () => {
+    const currentTitle = document.querySelector("#formBasicTitle").value;
+    const notification = document.querySelector("#error-title");
+    if (currentTitle.length === 0) {
+      notification.textContent = "This field is required";
+      return false;
+    }
+    if (currentTitle.length > 30) {
+      notification.textContent = "The title can't be longer than 30 characters";
+      return false;
+    }
+    notification.textContent = "";
+    return true;
+  }
+
+  const validDate = () => {
+    const currentDate = date;
+    const thisYear = new Date().getFullYear();
+    const notification = document.querySelector("#error-date");
+    if (currentDate == null) {
+      setDate(new Date());
+      notification.textContent = "Invalid date has been set to current date";
+      return true;
+    }
+    const currentYear = date.getFullYear();
+    if (currentYear > thisYear + 10) {
+      notification.textContent = `You shouldn't plan ${currentYear - thisYear} years ahead`;
+      return false;
+    }
+    notification.textContent = "";
+    return true;
+  }
+
+  const validateAmount = (e) => {
+    setAmount(e.target.value);
+    validAmount();
+  }
+
+  const validAmount = () => {
+    const inputAmount = document.querySelector("#formBasicAmount").value;
+    const currentAmount = parseInt(inputAmount);
+    const currentAmountFloat = parseFloat(inputAmount)
+    const notification = document.querySelector("#error-amount");
+    const pattern = "^\\d+$";
+    if (currentAmountFloat === 0 || inputAmount.length === 0) {
+      console.log(inputAmount);
+      notification.textContent = "This field is required";
+      return false
+    }
+    if (!inputAmount.match(pattern) || (currentAmountFloat > 0 && currentAmountFloat < 1)) {
+      notification.textContent = "This field must be an integer";
+      return false
+    }
+    if (currentAmount > 2147483647) {
+      notification.textContent = "The amount can't be higher than 2147483647";
+      return false
+    }
+    notification.textContent = "";
+    return true
+  }
+
 
   return (
     <>
@@ -46,11 +119,11 @@ export default function TransactionModal(props) {
         <Modal.Body>
           <TransactionForm
             title={title}
-            setTitle={(e) => setTitle(e.target.value)}
+            setTitle={validateTitle}
             date={date}
             setDate={setDate}
             amount={amount}
-            setAmount={(e) => setAmount(e.target.value)}
+            setAmount={validateAmount}
             frequency={frequency}
             setFrequency={(e) => setFrequency(e.target.value)}
           />
