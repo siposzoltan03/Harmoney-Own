6da29ee74e0531afd405f8b2772e8150cb166f0f
Márk Paróczi <mark.paroczi@gmail.com>
2020-03-18 14:25:59 +0100

    Open TransactionModal on click

--- src/components/TransactionCard/TransactionCard.js
+++ src/components/TransactionCard/TransactionCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from "styled-components";
 import {Card} from "react-bootstrap";
 import NumberFormater from "../../utils/NumberFormater";
@@ -7,17 +7,28 @@ import CloseSquareOutlined from "@ant-design/icons/lib/icons/CloseSquareOutlined
 import CaretDownOutlined from "@ant-design/icons/lib/icons/CaretDownOutlined";
 
 import "./TransactionCard.css"
+import {ModalVisibilityContext} from "../../contexts/ModalVisibilityContext";
 
 const Amount = styled.div`
     color: ${prop => prop.direction === "Income" ? 'green' : 'red'}
 `;
 
 function TransactionCard(transaction) {
+    const transactionModal = useContext(ModalVisibilityContext).transactionModal;
+    const transactionModalType = useContext(ModalVisibilityContext).transactionModalType;
+    const setTransactionModalIsVisible = transactionModal[1];
+    const setTransactionType = transactionModalType[1];
+
+    const editHandler = () => {
+        setTransactionType(transaction.direction);
+        setTransactionModalIsVisible(true);
+    };
+
     return (
-        <Card>
+        <Card onClick={editHandler}>
             <Card.Header as="h4">
                 <div className="transaction-title">{transaction.title}</div>
-                <div className="transaction-delete"><CloseSquareOutlined /></div>
+                <div className="transaction-delete" ><CloseSquareOutlined /></div>
             </Card.Header>
             <Card.Body as="h5">
                 <Amount className="transaction-direction" direction={transaction.direction}>{transaction.direction === "Income" ? <CaretUpOutlined /> : <CaretDownOutlined />}</Amount>
