0072a4297dad4f8bea185a145e343be2eed269ba
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-03-19 01:06:01 +0100

    change font size unit to vw, align center

--- src/utils/NumberFormatter.js
+++ src/utils/NumberFormatter.js
@@ -1,6 +1,6 @@
 const NumberFormater = {
   formatBalance: number => {
-    return number.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    return number.toFixed(0).replace(/\d(?=(\d{3})+\.)/g, "$&,");
   }
 };
 
