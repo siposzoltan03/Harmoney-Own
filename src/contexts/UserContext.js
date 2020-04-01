d3503e1e1c73edaad1f031591aff48c88d846c60
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-04-01 22:27:30 +0200

    Add getCurrentUser method for jwt auth

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,11 +5,17 @@ import Globals from "../utils/globals";
 const registrationUrl = Globals.fetchUrl + "/api/account/register";
 const loginUrl = Globals.fetchUrl + "/api/account/login";
 const logoutUrl = Globals.fetchUrl + "/api/account/logout";
+const currentUserUrl = Globals.fetchUrl + "/api/account/currentuser";
 
 export const UserContext = React.createContext(undefined, undefined);
 
 export const UserProvider = (props) => {
-    const [user, setUser] = useState();
+    let jwt = localStorage.getItem("token");
+    let currentUser;
+    if (jwt === null) currentUser = null;
+    else currentUser = getCurrentUser(jwt);
+
+    const [user, setUser] = useState(currentUser);
 
     const postRegistration = async (data) => {
         return await Axios.post(registrationUrl, data, { headers: {
@@ -25,11 +31,12 @@ export const UserProvider = (props) => {
     }
 
     const postLogin = async (data) => {
-        return await Axios.post(loginUrl, data, { headers: {
+        return await Axios.post(loginUrl, data, { withCredentials: true, headers: {
             'Content-Type': 'application/json',
         }})
         .then(resp => {
             if (resp.data.firstName && resp.data.lastName && resp.data.email && resp.data.email === JSON.parse(data).email) {
+                localStorage.setItem("token", resp.data.token);
                 setUser(resp.data);
                 return false;
             }
@@ -44,6 +51,7 @@ export const UserProvider = (props) => {
     const postLogout = async (data) => {
         return await Axios.post(logoutUrl, data, { headers: {
                 'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem("token")}`
             }})
             .then(resp => {
                 return false;
@@ -52,6 +60,15 @@ export const UserProvider = (props) => {
                 console.log('Error:', e);
                 return true;
             })
+    }
+
+    async function getCurrentUser(jwt){
+        if(jwt === null) return null;
+        return await Axios.post(currentUserUrl, {"tokenString": jwt},
+            {withCredentials:true,
+                headers:{'Content-Type': 'application/json',}})
+            .then(resp => setUser(resp.data))
+
     }
 
     return (
