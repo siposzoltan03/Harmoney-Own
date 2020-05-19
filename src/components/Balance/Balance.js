294a9772844c19ea75fb1e2ea81c679370c54d5e
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-05-19 12:20:18 +0200

    Redesign Balance with MUI

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,8 +1,6 @@
 import React, {useContext} from "react";
 import "./Balance.css";
 import './Balance.css';
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
 import Card from "react-bootstrap/Card";
 import NumberFormatter from "../../utils/NumberFormatter";
 import {TransactionContext} from "../../contexts/TransactionContext";
@@ -13,13 +11,15 @@ const Balance = () => {
     const transactions = appContext.transactions[0];
     let sum = 0;
     transactions.forEach(function (transaction) {
-        transaction.direction === 0 ? sum += transaction.amount : sum -= transaction.amount;
+        transaction.direction === 'Income' ? sum += transaction.amount : sum -= transaction.amount;
     });
     return (
-        <Paper elevation={1}>
-            <Typography variant={"h5"} content={"h4"}>Actual Balance</Typography>
-            <Typography className={"card-text"} variant={"h1"}>{NumberFormatter.formatBalance(sum)} Ft</Typography>
-        </Paper>
+        <Card>
+            <Card.Body>
+                <Card.Title>Actual Balance</Card.Title>
+                <Card.Text>{NumberFormatter.formatBalance(sum)} Ft</Card.Text>
+            </Card.Body>
+        </Card>
     );
 };
 
