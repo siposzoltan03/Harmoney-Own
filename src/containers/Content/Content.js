16216459d86205e4e5ea80c98b58f4ca977709a7
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-05-19 18:27:50 +0200

    Adjust Content to new routes

--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -1,14 +1,14 @@
 import React, {useContext} from 'react';
 import { UserContext } from "../../contexts/UserContext";
-import Dashboard from "../../components/Dashboard/Dashboard";
 import WelcomePage from "../../components/WelcomePage/WelcomePage";
+import Transactions from "../../components/Transactions/Transactions";
 
 function Content() {
     const { user } = useContext(UserContext);
     const userLoggedIn = user[0];
 
     if (userLoggedIn) {
-        return (<Dashboard/>)
+        return (<Transactions/>)
     } else {
         return (<WelcomePage />)
     }
