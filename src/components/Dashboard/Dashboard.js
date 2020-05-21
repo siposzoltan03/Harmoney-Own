f2e9784c8ec2cc7cc99266f50682c7a7fe0dccff
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-05-21 12:29:47 +0200

    Fix export issue

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -105,7 +105,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function Dashboard() {
+function Dashboard() {
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
     const handleDrawerOpen = () => {
