1cbd4e7c463b9de582c5dd8eeb41a3dc6e5205a6
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-05-22 15:06:25 +0200

    Change Sign In page Background image

--- src/containers/SignInPage.js
+++ src/containers/SignInPage.js
@@ -14,13 +14,15 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import {UserContext} from "../contexts/UserContext";
 import {userToJsonLogin} from "../utils/createjson";
+import SingInImage from "../assets/signinimage.jpg"
+
 
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
+            <Link color="inherit" to="/">
+                Harmoney
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
@@ -33,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
         height: '100vh',
     },
     image: {
-        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundImage: `url(${SingInImage})`,
         backgroundRepeat: 'no-repeat',
         backgroundColor:
             theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
@@ -140,7 +142,7 @@ export default function SignInPage() {
                         </Button>
                         <Grid container>
                             <Grid item xs>
-                                <Link href="#" variant="body2">
+                                <Link to="#" variant="body2">
                                     Forgot password?
                                 </Link>
                             </Grid>
