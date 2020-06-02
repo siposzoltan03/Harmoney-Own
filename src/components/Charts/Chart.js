21ff1757a5179cabe621f56cd2c1b2090c0279a0
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-06-02 17:08:09 +0200

    Fix current month display in title

--- src/components/Charts/Chart.js
+++ src/components/Charts/Chart.js
@@ -1,33 +1,18 @@
 import React, {useContext} from 'react';
-import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import {useTheme} from '@material-ui/core/styles';
+import {Label, Line, LineChart, ResponsiveContainer, XAxis, YAxis} from 'recharts';
 import Title from '../Title/Title';
 import {TransactionContext} from "../../contexts/TransactionContext"
 
-// Generate Sales Data
-// function createData(time, amount) {
-//     return { time, amount };
-// }
 
 function createData(date, amount) {
     return {date, amount}
 }
 
-const data = [
-    createData('00:00', 0),
-    createData('03:00', 300),
-    createData('06:00', 600),
-    createData('09:00', 800),
-    createData('12:00', 1500),
-    createData('15:00', 2000),
-    createData('18:00', 2400),
-    createData('21:00', 2400),
-    createData('24:00', undefined),
-];
-
 const getCurrentMonth = () => {
-    const options = {day: "numeric"};
-    return new Intl.DateTimeFormat('default', options).format(new Date());
+    const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    let now = new Date();
+    return months[now.getMonth()];
 };
 
 const getTransactionDatum = (transaction) => {
